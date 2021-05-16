a56230280134a997f71302fbd9b8c695191a5fae
mitiiuli <84138856+mitiiuli@users.noreply.github.com>
2021-05-16 21:04:44 +0300

    Update guess.js
    Fix indentation

--- guess.js
+++ guess.js
@@ -3,32 +3,41 @@ function addbutons()
 {
   var numberOfButtons = document.getElementById("text_dificulty").value;
   var numberButtons = parseInt(numberOfButtons);
-if (numberButtons <= 0) {
+  if (numberButtons <= 0) 
+  {
     document.getElementById("status").style.color = "red";
     document.getElementById("status").innerHTML = "Status: Please restart and write another number!";
-  } else if (numberButtons == 1) {
+  } else if (numberButtons == 1) 
+  {
     document.getElementById("status").style.color = "red";
     document.getElementById("status").innerHTML = "Status: It's too hard! Please restart!";
-  } else {
+  } 
+  else 
+  {
     winningButton = Math.floor(Math.random() * numberButtons) + 1;
-    for (var i = 1; i <= numberButtons; i++) {
+    for (var i = 1; i <= numberButtons; i++) 
+    {
       $('#buttons').append(`
-        <button type="button" id = "` + i + `" class="btn btn-secondary btn-lg" onclick = "return guessButton(` + i + `);">Button: ` + i + `</button>
-      `);
+        <button type="button" id = "` + i + `" class="btn btn-secondary btn-lg" onclick = "return guessButton(` + i + `);">Button: ` + i + `</button>`);
     }
       return false;
   }
 }
 
-function guessButton(id) {
-  if (id == winningButton) {
+function guessButton(id) 
+{
+  if (id == winningButton) 
+  {
     document.getElementById("status").style.color = "green";
     document.getElementById("status").innerHTML = "Status: Winner!!!";
-  } else {
+  } 
+  else 
+  {
     document.getElementById("status").style.color = "red";
     document.getElementById("status").innerHTML = "Status: Loser!!!";
   }
 }
-function restartGame() {
+function restartGame() 
+{
   location.reload();
 }
